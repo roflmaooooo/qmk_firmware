52aabef9b110727798760d0ce76df063b9700ce0
roflmaooooo <levinfrias@gmail.com>
2024-10-06 03:23:05 +0800

    Added new oled screen to support six layers; added six-layer keymap

--- keyboards/doio/kb16/keymaps/default/rules.mk
+++ keyboards/doio/kb16/keymaps/default/rules.mk
@@ -1,2 +1,4 @@
+SRC += ./lib/layer_status/layer_status.c
+
 # Encoder enabled
 ENCODER_MAP_ENABLE = yes
---
+++ keyboards/doio/kb16/keymaps/six_layers/rules.mk
@@ -0,0 +1,4 @@
+SRC += ./lib/layer_status_six_layers/layer_status.c
+
+# Encoder enabled
+ENCODER_MAP_ENABLE = yes
